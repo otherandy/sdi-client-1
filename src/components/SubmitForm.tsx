0e58227d314f480cb794d1fbc25157cd235c02c6
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2024-10-19 23:03:28 -0700

    added spinner

--- src/components/SubmitForm.tsx
+++ src/components/SubmitForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:4000";
 
 interface LocationData {
@@ -12,6 +14,8 @@ interface WeatherData {
 }
 
 export default function Submit() {
+  const [loading, setLoading] = useState(false);
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
@@ -44,6 +48,7 @@ export default function Submit() {
     };
 
     try {
+      setLoading(true);
       await fetch(`${serverUrl}/content`, {
         method: "POST",
         headers: {
@@ -54,6 +59,8 @@ export default function Submit() {
     } catch (error) {
       console.error(error);
     }
+
+    setLoading(false);
   };
 
   return (
@@ -61,7 +68,34 @@ export default function Submit() {
       <input type="text" id="title" placeholder="Title" />
       <textarea id="content" placeholder="Content" />
       <button type="submit" className="submit">
-        Submit
+        {!loading ? (
+          "Submit"
+        ) : (
+          <svg
+            className="spinner"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              style={{
+                opacity: 0.25,
+              }}
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              style={{
+                opacity: 0.75,
+              }}
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        )}
       </button>
     </form>
   );
